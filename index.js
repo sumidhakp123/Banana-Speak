c5a781b3651f6e1f66f74c6c1ddbffb6ffd12e08
Sumidha KP <kumari.sumidha.123@gmail.com>
2021-07-27 14:05:15 +0530

    taking input and console

---
+++ index.js
@@ -0,0 +1,7 @@
+var btnTranslate=document.querySelector("#btn-translate")
+
+btnTranslate.addEventListener("click", function clickHandler(){
+    console.log("clicked!");
+    console.log("input", txtInput.value)
+})
+var txtInput=document.querySelector("#txt-input")
